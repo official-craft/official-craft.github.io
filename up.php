714a592e142320d5133cb022e21063519b04ae34
official-craft <59807804+official-craft@users.noreply.github.com>
2022-03-14 17:44:04 -0700

    Update up.php

--- up.php
+++ up.php
@@ -1,5 +1,5 @@
 <?php
-$secret_key = "KEYGOESHERE"; //Set this as your secret key, to prevent others uploading to your server.
+$secret_key = "test"; //Set this as your secret key, to prevent others uploading to your server.
 $sharexdir = "images/"; //This is your file dir, also the link..
 $domain_url = 'https://officialcraft.org/'; //Add an S at the end of HTTP if you have a SSL certificate.
 $lengthofstring = 5; //Length of the file name
